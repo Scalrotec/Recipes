f6df8605a76ba014089c4e0c5b114fdea10aad94
Scalrotec <calebrotichke@gmail.com>
2026-07-18 23:54:53 +0300

    update

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,8 @@ function About() {
   return (
     <div>
       <h1>About Us</h1>
-      <p>Welcome to our about page!</p>
+      <p>You are very lucky to be here! We make Recipes of all Kind </p>
+      <p> Check our Amazing Recipes !!!</p>
     </div>
   );
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,6 +3,7 @@ function Contact() {
     <div>
       <h1>Contact Us</h1>
       <p>Get in touch with us!</p>
+      <p>Email: Example@gmail.com</p>
     </div>
   );
 }
