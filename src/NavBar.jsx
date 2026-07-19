66c88d8749e4b3883d2410963629489bb53a1dd4
Scalrotec <calebrotichke@gmail.com>
2026-07-19 20:45:21 +0300

    logo

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,6 +6,11 @@ const NavBar = () => {
   return (
     <div>
       <nav className="navbar">
+        <img
+          style={{ width: "100px", height: "50px" }}
+          src="/recipe-logo.png"
+          alt="Recipe"
+        />
         <ul>
           <li>
             <NavLink to="/">Home</NavLink>
